6635df00d43769001c409a242e6daae86d5536b9
bhupendragtm <bhupendragtm777@gmail.com>
2022-12-08 15:18:36 +0000

    Show SetShow Completed

--- src/component/Accordian/MyAccordian.js
+++ src/component/Accordian/MyAccordian.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 const MyAccordian = ({ data }) => {
   const [show, setShow] = useState(false);
   return (
-    <><div><p>+</p>
+    <><div><p onClick={()=>setShow(!show)}>+</p>
       <h1>{data.question}</h1></div>
      {show &&<h1>{data.answer}</h1>}
     </>
